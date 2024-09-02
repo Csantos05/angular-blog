3e46b8685ec22d6e6f05d0a673079528d45817ce
Cleison Santos <cleisonsantosrj79@gmail.com>
2024-09-02 16:21:06 -0300

    components atualizados

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  fotoCover:string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM9iIlngAstzHI4l1VRS-HodjRF7VAAqCW0Fef4ltFdzSzXH1-oNmhmtS4Ztz0gc5xnpw&usqp=CAU"
+  @Input()
+  cardTitle:string = "Points"
+
+  
 
   constructor() { }
 
